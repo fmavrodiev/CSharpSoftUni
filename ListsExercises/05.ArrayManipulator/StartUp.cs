e406181cddbef313aed0cb5a32b7356626fa4031
fmavrodiev <fmavrodiev@gmail.com>
2017-10-17 21:50:56 +0300

    ...solutionFive

--- ListsExercises/05.ArrayManipulator/StartUp.cs
+++ ListsExercises/05.ArrayManipulator/StartUp.cs
@@ -17,8 +17,7 @@
             {
                 var commandArgs = command
                     .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToList();
+                    .ToArray();
                 if (commandArgs[0] == "add")
                 {
                     numbers.Insert(int.Parse(commandArgs[1]), int.Parse(commandArgs[2]));
@@ -26,7 +25,35 @@
                 }
                 else if (commandArgs[0] == "addMany")
                 {
-
+                    numbers.InsertRange(int.Parse(commandArgs[1]), commandArgs.Skip(2).Select(int.Parse).ToArray());
+                }
+                else if (commandArgs[0] == "contains")
+                {
+                    int number = int.Parse(commandArgs[1]);
+                    Console.WriteLine(numbers.IndexOf(number));
+                }
+                else if (commandArgs[0] == "remove")
+                {
+                    numbers.RemoveAt(int.Parse(commandArgs[1]));
+                }
+                else if (commandArgs[0] == "shift")
+                {
+                    int number = int.Parse(commandArgs[1]);
+                    number = number % numbers.Count;
+                    for (int i = 0; i < number; i++)
+                    {
+                        numbers.Add(numbers[0]);
+                        numbers.RemoveAt(0);
+                    }
+                }
+                else if (commandArgs[0] == "sumPairs")
+                {
+                    for (int i = 0; i < numbers.Count -1; i++)
+                    {
+                        var sum = numbers[i] + numbers[i] + 1;
+                        numbers[i] = sum;
+                        numbers.RemoveAt(i + 1);
+                    }
                 }
                 command = Console.ReadLine();
             }
